46b0d83c93f1efc85d337ead9a5ab14e47e8d168
James Manalang <james.e.manalang@gmail.com>
2022-05-13 02:21:23 +0800

    Add tailwind css and flex layout with sticky sidebar

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Card from './components/Card';
 
 function App() {
   return (
-    <div>
-      <h1>Product Feedback App</h1>
+    <div className="container">
+      <div className="flex h-screen gap-8 pt-20">
+        <aside className="w-64 flex-shrink-0 max-w-xs flex flex-col">
+          <div className="sticky top-20">
+            <Card>
+              <h1 className="text-lg font-bold">Aside</h1>
+            </Card>
+          </div>
+        </aside>
+        <main className="flex-1">
+          <Card>
+            <h1>Main</h1>
+          </Card>
+          <Card>
+            <h1>Main</h1>
+          </Card>
+          <Card>
+            <h1>Main</h1>
+          </Card>
+          <Card>
+            <h1>Main</h1>
+          </Card>
+          <Card>
+            <h1>Main</h1>
+          </Card>
+          <Card>
+            <h1>Main</h1>
+          </Card>
+          <Card>
+            <h1>Main</h1>
+          </Card>
+          <Card>
+            <h1>Main</h1>
+          </Card>
+          <Card>
+            <h1>Main</h1>
+          </Card>
+          <Card>
+            <h1>Main</h1>
+          </Card>
+          <Card>
+            <h1>Main</h1>
+          </Card>
+          <Card>
+            <h1>Main</h1>
+          </Card>
+          <Card>
+            <h1>Main</h1>
+          </Card>
+          <Card>
+            <h1>Main</h1>
+          </Card>
+        </main>
+      </div>
     </div>
   );
 }
---
+++ client/tailwind.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    container: {
+      center: true,
+      padding: '21px',
+    },
+    colors: {
+      white: '#FFFFFF',
+      purple: '#AD1FEA',
+      blue: '#4661E6',
+      navy: '#373F68',
+      smoke: '#F2F4FF',
+      light: '#F7F8FD',
+      midnight: '#3A4374',
+      slate: '#647196',
+      orange: '#F49F85',
+      sky: '#62BCFA',
+    },
+    fontFamily: {
+      body: ['Jost', 'sans-serif'],
+    },
+    borderRadius: {
+      DEFAULT: '10px',
+    },
+  },
+};
